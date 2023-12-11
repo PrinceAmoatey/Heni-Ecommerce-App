b372ab93bf9b1a41d6cbec8d949208d4430cdb96
Prince Cobby Amoatey <princecobbyamoatey@PAC.local>
2023-12-11 17:43:32 +0000

    page structure 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import NavBar from './components/Nav/NavBar'
+import Footer from './components/footer/Footer'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ["400", "700"] })
 
@@ -17,7 +19,17 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+
+        <div className="flex flex-grow min-h-screen"> 
+        <NavBar />
+       <main className="flex-grow">{children}</main>
+        <Footer /> 
+
+        </div>
+
+        
+        </body>
     </html>
   )
 }
